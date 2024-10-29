f748eaa6eb60e052f13fec7a2f33c40ba9c8f832
suonianjiexinghe520 <z2381982822@gmail>
2024-10-29 17:43:45 +0800

    change index.html

--- vits2_api.py
+++ vits2_api.py
@@ -251,7 +251,7 @@ class GenerateResponse(BaseModel):
 async def root():
     service_name = """
         <html> <head> <title>tts_service</title> </head>
-            <body style="display: flex; justify-content: center;"> <h1> vits_service_api</h1></body> </html>
+            <body style="display: flex; justify-content: center;"> <h1> vits2_service_api</h1></body> </html>
         """
     return HTMLResponse(content=service_name, status_code=200)
 
