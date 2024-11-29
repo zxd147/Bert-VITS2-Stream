15fc021a75aa482f2b0dc132c0582da82b1a7a8f
suonianjiexinghe520 <z2381982822@gmail>
2024-11-29 19:28:31 +0800

    add log of refresh model

--- vits2_api.py
+++ vits2_api.py
@@ -141,7 +141,7 @@ def refresh_models(speaker, language):
     global models_map, hps_map, device_map, request_count, current_speaker, current_language  # 声明全局变量
     if speaker != current_speaker or not models_map:
         current_speaker = speaker
-        vits_logger.info("检测到speaker发生改变，重新加载模型...")
+        vits_logger.info(f"检测到speaker发生改变，重新加载模型{speaker, language}...")
         device_map, models_map, hps_map = get_models(models_config, speaker, language)
     elif current_language == 'mix':
         # 检查请求的语言是否不是 'mix'
@@ -155,7 +155,7 @@ def refresh_models(speaker, language):
         else:
             request_count = 0  # 重置请求计数器
     elif language == 'mix':
-        vits_logger.info("检测到language发生改变，重新加载模型...")
+        vits_logger.info(f"检测到language发生改变，重新加载模型{speaker, language}...")
         current_language = language
         device_map, models_map, hps_map = get_models(models_config, speaker, language)
     return device_map, models_map, hps_map
